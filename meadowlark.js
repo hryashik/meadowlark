26e10b57bc049eeaf3a790a773adb0b7eefd2350
Dmitriy <tsukikohaya@gmail.com>
2022-03-27 13:21:32 +0300

    Добавил динамический контент во view (about.hbs)

--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,13 @@ const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const port = 3000
 const app = express()
+const fortunes = [
+   "Победи свои страхи, или они победят тебя.",
+   "Рекам нужны истоки.",
+   "Не бойся неведомого.",
+   "Тебя ждет приятный сюрприз.",
+   "Будь проще везде, где только можно."
+]
 
 app.engine('hbs', expressHandlebars.engine({
    defaultLayout: 'main',
@@ -15,14 +22,16 @@ app.get('/', ((req, res) => {
    res.render('home')
 }))
 app.get('/about', (req, res) => {
-   res.render('about')
+   let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+   res.render('about', {
+      fortune: randomFortune
+   })
 })
 
 app.use((req, res) => {
    res.status(404)
    res.render('404')
 })
-
 
 
 app.listen(port, () => console.log('Express is starting...'))
